3656cf6b4c0e0f30587357e577004b329b250e97
mikiraj <miracoreren@gmail.com>
2023-05-10 03:20:06 +0300

    i dont know the where is the mistake

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-console.log("Hi");
+console.log("Hello World");
 
 /*
 ! need to learn how to take input
@@ -10,7 +10,7 @@ first function getComputerChoice
 
  
 
- function getComputerChoice() 
+ function GetComputerChoice() 
  {
 
     /*We will use this random number generator 0-2 to choose between r/p/s */
@@ -24,21 +24,94 @@ first function getComputerChoice
     switch(getRandomInt())
     {
       case 0: 
-        choice= "rock";
+        choise= "rock";
         break;
       case 1: 
-        choice= "paper";
+        choise= "paper";
         break;
       case 2: 
-      choice= "sciccors"; 
+      choise= "sciccors"; 
       break;
       default:
-        choice= "There is a error!";
+        choise= "There is a error!";
     }
 
 
 
 
-  return choice;
+  return choise;
 }
-console.log( getComputerChoice() )
+
+
+
+/* console.log( getComputerChoice() ) */
+
+
+function game(){
+
+  let personaChoise = prompt("Rock,papper or scissors?");
+  console.log(personaChoise);
+
+
+  let getComputerChoice=  GetComputerChoice();
+  let numberOfGame =5
+  let win=0;
+  let loose=0;
+  for(let i=0; i<numberOfGame; i++){
+    
+    let personaChoise = prompt("Rock/papper/scissor?");
+    let compDe        = getComputerChoice();
+    let per  = personaChoise.toUpperCase();
+    let comp = compDe.toUpperCase();
+
+    if(per=comp){/* if comp and person choose the same option game must have more rounds to finish the game */
+      numberOfGame++;
+    }else if(per = "ROCK"){
+      if(comp= "PAPER"){
+        console.log("You lost this round. BOOHOO! Computer choosed "+comp);
+        loose++;
+      }else if(comp= "SCISSORS"){
+        console.log("You won this round. Yeeeay! Computer choosed "+comp);
+        win++;
+      }
+
+
+    }else if(per = "PAPER"){
+      if(comp= "SCISSORS"){
+      console.log("You lost this round. BOOHOO! Computer choosed "+comp);
+      loose++;
+    }else if(comp= "ROCK"){
+      console.log("You won this round. Yeeeay! Computer choosed "+comp);
+      win++;
+    }
+
+  }else if(per = "SCISSORS"){
+      if(comp= "ROCK"){
+      console.log("You lost this round. BOOHOO! Computer choosed "+comp );
+      loose++;
+    }else if(comp= "PAPER"){
+      console.log("You won this round. Yeeeay! Computer choosed "+comp );
+      win++;
+    }
+
+  }
+
+  if(win>loose){
+    console.log("YOU WON THE GAME. GOOD BOY/GIRL!")
+  }else if(loose>win){
+    console.log("YOU LOST THE GAME. THAT'S OKAY!!");
+  }
+}
+/*
+console.log("let's begin");
+game();*/
+
+
+
+
+
+
+
+
+
+
